f1e978c32f2ad25b6c36d4af36aa581c8b23810d
Julien-MANSOUR <j8mansou@enib.fr>
2020-12-09 16:35:52 +0100

    adding scores and images

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -4,7 +4,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPixmap,QFont
 from PyQt5.QtCore import QTimer
 
-font=QFont("Times",12)
+textFont=QFont("Times",16)
+buttonFont=QFont("Arial",12)
 class Window (QWidget):
     def __init__(self):
         super().__init__()
@@ -13,6 +14,29 @@ class Window (QWidget):
         self.UI()
 
     def UI(self):
+        ########################Score########################
+        self.scoreComputer=QLabel("Computer Score : ",self)
+        self.scoreComputer.move(30,20)
+        self.scoreComputer.setFont(textFont)
+        self.scoreComputer.setStyleSheet("background-color:red")
+        self.scorePlayer=QLabel("Your Score : ",self)
+        self.scorePlayer.move(330,20)
+        self.scorePlayer.setFont(textFont)
+        self.scorePlayer.setStyleSheet("background-color:blue")
+        ########################Image########################
+        self.ComputerImage=QLabel(self)
+        self.ComputerImage.setPixmap(QPixmap("images/rock.png"))
+        self.ComputerImage.move(50,100)
+
+        self.PlayerImage=QLabel(self)
+        self.PlayerImage.setPixmap(QPixmap("images/rock.png"))
+        self.PlayerImage.move(330,100)
+
+        self.GameImage=QLabel(self)
+        self.GameImage.setPixmap(QPixmap("images/game.png"))
+        self.GameImage.move(235,160)
+
+
         self.show()
 
 
