7a95674fab0c135b97661292f9c7acb2e1e74c91
Julien-MANSOUR <j8mansou@enib.fr>
2020-12-09 19:47:11 +0100

    Making the stop funct : all probabilities of the values/scores/final result

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -6,23 +6,25 @@ from PyQt5.QtCore import QTimer
 from random import randint
 textFont=QFont("Times",16)
 buttonFont=QFont("Arial",12)
+PlayerScore = 0
+ComputerScore = 0
 class Window (QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300,300,500,500)
+        self.setGeometry(300,300,550,500)
         self.setWindowTitle("mosso")
         self.UI()
 
     def UI(self):
         ########################Score########################
-        self.scoreComputer=QLabel("Computer Score : ",self)
-        self.scoreComputer.move(30,20)
-        self.scoreComputer.setFont(textFont)
-        self.scoreComputer.setStyleSheet("background-color:red")
-        self.scorePlayer=QLabel("Your Score : ",self)
-        self.scorePlayer.move(330,20)
-        self.scorePlayer.setFont(textFont)
-        self.scorePlayer.setStyleSheet("background-color:blue")
+        self.scoreComputerText=QLabel("Computer Score : ",self)
+        self.scoreComputerText.move(30,20)
+        self.scoreComputerText.setFont(textFont)
+        self.scoreComputerText.setStyleSheet("background-color:red")
+        self.scorePlayerText=QLabel("Your Score : ",self)
+        self.scorePlayerText.move(330,20)
+        self.scorePlayerText.setFont(textFont)
+        self.scorePlayerText.setStyleSheet("background-color:blue")
         ########################Image########################
         self.ComputerImage=QLabel(self)
         self.ComputerImage.setPixmap(QPixmap("images/rock.png"))
@@ -56,14 +58,16 @@ class Window (QWidget):
 
     def PlayGame(self):
         self.randComputerNmb=randint(1,3)#rock,paper,sicors
+        self.randPlayerNmb=randint(1,3)
+        print(self.randComputerNmb,self.randPlayerNmb)
         if self.randComputerNmb == 1:
             self.ComputerImage.setPixmap(QPixmap("images/rock.png"))
         elif self.randComputerNmb == 2:
             self.ComputerImage.setPixmap(QPixmap("images/paper.png"))
         else:
-            self.ComputerImage.setPixmap(QPixmap("image/scissors.png"))
+            self.ComputerImage.setPixmap(QPixmap("images/scissors.png"))
 
-        self.randPlayerNmb=randint(1,3)
+
         if self.randPlayerNmb==1:
             self.PlayerImage.setPixmap(QPixmap("images/rock.png"))
         elif self.randPlayerNmb==2:
@@ -72,7 +76,51 @@ class Window (QWidget):
             self.PlayerImage.setPixmap(QPixmap("images/scissors.png"))
 
     def stop(self):
+        global PlayerScore
+        global ComputerScore
+
         self.timer.stop()
+        if self.randComputerNmb == self.randPlayerNmb:
+            msg=QMessageBox.information(self,"Information","It is a Draw")
+        elif self.randComputerNmb == 1 and self.randPlayerNmb == 2:  #rock vs paper
+            msg=QMessageBox.information(self,"Information","you Win !")
+            PlayerScore += 1
+            self.scorePlayerText.setText("Your Score : {}".format(PlayerScore))
+            self.scorePlayerText.resize(150,30)
+        elif self.randComputerNmb == 1 and self.randPlayerNmb == 3:  #rock vs scissor
+            msg=QMessageBox.information(self,"Information","Computer Wins !")
+            ComputerScore += 1
+            self.scoreComputerText.setText("Computer Score : {}".format(ComputerScore))
+            self.scoreComputerText.resize(200,30)
+        elif self.randComputerNmb == 2 and self.randPlayerNmb == 1: #paper vs rock
+            msg=QMessageBox.information(self,"Information","Computer Wins !")
+            ComputerScore += 1
+            self.scoreComputerText.setText("Computer Score : {}".format(ComputerScore))
+            self.scoreComputerText.resize(200,30)
+        elif self.randComputerNmb == 2 and self.randPlayerNmb == 3: #paper vs scissours
+            msg=QMessageBox.information(self,"Information","you Win !")
+            PlayerScore += 1
+            self.scorePlayerText.setText("Your Score : {}".format(PlayerScore))
+            self.scorePlayerText.resize(150,30)
+        elif self.randComputerNmb == 3 and self.randPlayerNmb== 1:  #scisours vs rock
+            msg=QMessageBox.information(self,"Information","you Win !")
+            PlayerScore += 1
+            self.scorePlayerText.setText("Your Score : {}".format(PlayerScore))
+            self.scorePlayerText.resize(150,30)
+        elif  self.randComputerNmb == 3 and self.randPlayerNmb == 2: #scissors vs paper 
+            msg=QMessageBox.information(self,"Information","Computer Wins !")
+            ComputerScore += 1
+            self.scoreComputerText.setText("Computer Score : {}".format(ComputerScore))
+            self.scoreComputerText.resize(200,30)
+
+        if PlayerScore == 3 or ComputerScore == 3 :
+            if PlayerScore == 3:
+                msg=QMessageBox.information(self,"Information","You Won ! \n CONGRATS!!")
+            else:
+                msg=QMessageBox.information(self,"Information","Computer WINS !! \n Game over!!")
+            sys.exit(1)
+
+
 
 
 def main():
